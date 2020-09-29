4f1917f24b280472669627234649f22008245b89
arifusr <arifusr@gmail.com>
2020-09-29 16:14:52 +0700

    origin data schema

--- src/schema/origin_data.schema.js
+++ src/schema/origin_data.schema.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 module.exports = [{
     customer_name: {
         type: String,
@@ -27,13 +30,45 @@ module.exports = [{
         type: String,
         validate:{
             validator: function(v) {
+                if(v === null || v === undefined) return true
                 return /^([0-9]|-){4,11}/.test(v);
               },
               message: props => `${props.value} is not a valid phone number, example 024-1234567`
         },
         maxlength:11,
-        default: ""
-    }
+        default: null
+    },
+    customer_address_detail:{
+        type: String,
+        maxlength: 100,
+        default:""
+    },
+    customer_zip_code:{
+        type: String,
+        validate:{
+          validator: function(v) {
+            return /^[0-9]+$/.test(v);
+          },
+          message: props => `${props.value} is not a valid zip code`
+        },
+        minlength:5,
+        maxlength: 5
+    },
+    zone_code:{
+        type: String,
+        enum: ['CGKFT','SMG', 'BDG'],
+        required: true
+    },
+    organization_id:{
+        type: Number,
+        min:1,
+        max:9999999,
+        required: true
+      },
+      location_id:{
+        type:  Schema.Types.ObjectId,
+        required: true
+      },
   }, {
     _id : false,
   }]
